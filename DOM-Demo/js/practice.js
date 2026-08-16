164e32b4a014765948ab792b383a22fd50cafa78
Bharvadiya Aaryan V <aryanbharvadiya93@gmail.com>
2026-08-16 11:14:33 +0530

    no changes

--- DOM-Demo/js/practice.js
+++ DOM-Demo/js/practice.js
@@ -1,22 +1,49 @@
-const display = document.getElementById("display");
+// const display = document.getElementById("display");
 
-function appendValue(value){
-    display.value += value;
+// function appendValue(value){
+//     display.value += value;
+// }
+
+// function clearDisplay(){
+//     display.value = "";
+// }
+
+// function backspace(){
+//     display.value = display.value.slice(0,-1);
+// }
+
+// function calculate(){
+//     try{
+//         display.value = eval(display.value);
+//     }catch{
+//         display.value = "Error";
+//         }
+// }
+
+
+let string = prompt("Enter the string: ");
+let rev="";
+
+
+for(let i=string.length - 1; i>=0 ; i--){
+    rev = rev + string[i];
 }
 
-function clearDisplay(){
-    display.value = "";
+console.log(rev);
+if(rev == string){
+    console.log("It is palindrome");
+}
+else
+{
+    console.log("It is not palindrome");
 }
 
-function backspace(){
-    display.value = display.value.slice(0,-1);
+let count = 0;
+
+for(val of string){
+    if(val == "a" | val == "i" | val == "o" | val == "u" | val == "e"){
+       count++;
+    }
 }
 
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    }catch{
-        display.value = "Error";
-        }
-}
-
+console.log(count);
